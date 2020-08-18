c8f6c6f3af917eb553a0c4722987c27a9c3c4af9
Mark Raffy Bahalla <mark@recph.com>
2020-08-18 22:18:06 +0800

    Added inner pages

--- js/index.js
+++ js/index.js
@@ -9,4 +9,10 @@ $(document).ready(function () {
         event.preventDefault();
         $(this).ekkoLightbox();
     });
+
+    $('.navbar .dropdown').hover(function () {
+        $(this).find('.dropdown-menu').first().stop(true, true).delay(0).slideDown();
+    }, function () {
+        $(this).find('.dropdown-menu').first().stop(true, true).delay(50).slideUp()
+    });
 })
