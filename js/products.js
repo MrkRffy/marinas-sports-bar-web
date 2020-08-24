f7706cb54d7462860fd43df642d5189fd2a8cddb
Mark Raffy Bahalla <mark@recph.com>
2020-08-24 19:43:57 +0800

    Separated menu gallery and menu list, changed menu list layout and implementation, changed menu gallery layout

--- js/products.js
+++ js/products.js
@@ -1,16 +1,12 @@
-function changeStyle(style) {
-    if (style == 'grid') {
-        $(".products-container .grid").show()
-        $(".products-container .list").hide();
+function selectCategory(element, category) {
+    $(".category-option").removeClass("active");
+    $(element).addClass("active");
 
-        $(".products-style-selector .grid-style").addClass("active");
-        $(".products-style-selector .list-style").removeClass("active");
-    } else {
-        $(".products-container .grid").hide()
-        $(".products-container .list").show();
-
-        $(".products-style-selector .list-style").addClass("active");
-        $(".products-style-selector .grid-style").removeClass("active");
+    if (category == 'all') {
+        $(".product-item").parent().fadeIn();
+        return;
     }
 
+    $(".product-item").parent().hide();
+    $(".product-item." + category).parent().fadeIn(1000);
 }
